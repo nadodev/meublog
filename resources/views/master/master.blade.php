fcdc2ea0e7f781db6a37760c4402df7bbf7d1aa2
leoanrdo <programacao@studioalpha.com.br>
2024-05-17 15:46:47 -0300

    fix: adjuste width container

--- resources/views/master/master.blade.php
+++ resources/views/master/master.blade.php
@@ -8,7 +8,7 @@
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/remixicon/4.2.0/remixicon.css" integrity="sha512-OQDNdI5rpnZ0BRhhJc+btbbtnxaj+LdQFeh0V9/igiEPDiWE2fG+ZsXl0JEH+bjXKPJ3zcXqNyP4/F/NegVdZg==" crossorigin="anonymous" referrerpolicy="no-referrer" />
 </head>
 <body>
-    <div class="container">
+    <div class="container-2xl">
         @yield('content')
     </div>
 </body>

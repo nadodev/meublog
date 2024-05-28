8b00f399ed3e3cc0cb43faded990925328827774
Leonardo Geja <bemmenos@gmail.com>
2024-05-28 12:43:36 -0300

    ajustado link curriculo da home

--- resources/views/home/index.blade.php
+++ resources/views/home/index.blade.php
@@ -48,7 +48,7 @@
                     <h1>Crafting visually web experiences with a focus
                         <span><strong>on design excellence.</strong></span>
                     </h1>
-                    <a href="/resume.pdf" target="_blank" rel="noopener noreferrer" class="btn-myresume">My resume
+                    <a href="/site/resume.pdf" target="_blank" rel="noopener noreferrer" class="btn-myresume">My resume
                         <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
                             stroke="currentColor" class="w-6 h-6">
                             <path stroke-linecap="round" stroke-linejoin="round" d="m4.5 4.5 15 15m0 0V8.25m0 11.25H8.25" />

4addf8822cd6ce3022bb2c497c1228d2137b445c
PC GAMer <bemmenos@gmail.com>
2025-03-01 13:45:46 -0300

    fix: traduz os arquivos

--- resources/views/home/index.blade.php
+++ resources/views/home/index.blade.php
@@ -22,7 +22,7 @@
                                     d="M15.59 14.37a6 6 0 0 1-5.84 7.38v-4.8m5.84-2.58a14.98 14.98 0 0 0 6.16-12.12A14.98 14.98 0 0 0 9.631 8.41m5.96 5.96a14.926 14.926 0 0 1-5.841 2.58m-.119-8.54a6 6 0 0 0-7.381 5.84h4.8m2.581-5.84a14.927 14.927 0 0 0-2.58 5.84m2.699 2.7c-.103.021-.207.041-.311.06a15.09 15.09 0 0 1-2.448-2.448 14.9 14.9 0 0 1 .06-.312m-2.24 2.39a4.493 4.493 0 0 0-1.757 4.306 4.493 4.493 0 0 0 4.306-1.758M16.5 9a1.5 1.5 0 1 1-3 0 1.5 1.5 0 0 1 3 0Z" />
                             </svg>
 
-                            Lets talk
+                            Vamos conversar?
                         </a>
 
                         {{-- <div class="language">
@@ -47,12 +47,12 @@
         <section class="hero">
             <div class="container-2xl">
                 <div class="hero__content">
-                    <span class="subtitle">Hello! I’m Leonardo Geja.</span>
-                    <h1>Crafting visually web experiences with a focus
-                        <span><strong>on design excellence.</strong></span>
+                    <span class="subtitle">Olá! eu sou Leonardo Geja.</span>
+                    <h1>Criando interfaces visuais na web com foco
+                        <span><strong> na excelência do usuário.</strong></span>
                     </h1>
-                    <a href="{{ asset('resume.pdf') }}" target="_blank" rel="noopener noreferrer" class="btn-myresume">My
-                        resume
+                    <a href="{{ asset('resume.pdf') }}" target="_blank" rel="noopener noreferrer" class="btn-myresume">Meu
+                    Currículo
                         <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
                             stroke="currentColor" class="w-6 h-6">
                             <path stroke-linecap="round" stroke-linejoin="round" d="m4.5 4.5 15 15m0 0V8.25m0 11.25H8.25" />
@@ -71,8 +71,8 @@
         <section class="approch">
             <div class="container-2xl">
                 <div class="approch__header flex flex-col  md:flex-row w-full items-center">
-                    <h2>What is my workflow</h2>
-                    <span>The approach</span>
+                    <h2>Qual é o meu fluxo de trabalho?</h2>
+                    <span>A abordagem</span>
                 </div>
                 <div class="approch__wrapper">
                     <div class="approch__item">
@@ -82,11 +82,9 @@
                                 <path stroke-linecap="round" stroke-linejoin="round"
                                     d="M12 18v-5.25m0 0a6.01 6.01 0 0 0 1.5-.189m-1.5.189a6.01 6.01 0 0 1-1.5-.189m3.75 7.478a12.06 12.06 0 0 1-4.5 0m3.75 2.383a14.406 14.406 0 0 1-3 0M14.25 18v-.192c0-.983.658-1.823 1.508-2.316a7.5 7.5 0 1 0-7.517 0c.85.493 1.509 1.333 1.509 2.316V18" />
                             </svg>
-                            Strategy
+                            Estratégia
                         </h3>
-                        <p class="text-md">In this stage, I identify and assess which problem needs to be resolved,
-                            determine the
-                            project requirements, and establish the optimal architecture for implementation.</p>
+                        <p class="text-md">Nesta etapa, eu identifico e avalio qual problema precisa ser resolvido, determino os requisitos do projeto e estabeleço a arquitetura ideal para a implementação.</p>
                     </div>
                     <div class="approch__item">
                         <h3 class="font-black text-2xl">
@@ -96,11 +94,9 @@
                                     d="m6.75 7.5 3 2.25-3 2.25m4.5 0h3m-9 8.25h13.5A2.25 2.25 0 0 0 21 18V6a2.25 2.25 0 0 0-2.25-2.25H5.25A2.25 2.25 0 0 0 3 6v12a2.25 2.25 0 0 0 2.25 2.25Z" />
                             </svg>
 
-                            Execution
+                            Execução
                         </h3>
-                        <p>In this phase, I implement the previously defined strategy, breaking it down into smaller
-                            tasks and ensuring that all requirements and structures are optimized to the best of my
-                            ability.</p>
+                        <p>Nesta fase, implemento a estratégia definida anteriormente, dividindo-a em tarefas menores e garantindo que todos os requisitos e estruturas sejam otimizados da melhor forma possível.</p>
                     </div>
                     <div class="approch__item">
                         <h3 class="font-black text-2xl">
@@ -110,10 +106,9 @@
                                     d="M3 8.25V18a2.25 2.25 0 0 0 2.25 2.25h13.5A2.25 2.25 0 0 0 21 18V8.25m-18 0V6a2.25 2.25 0 0 1 2.25-2.25h13.5A2.25 2.25 0 0 1 21 6v2.25m-18 0h18M5.25 6h.008v.008H5.25V6ZM7.5 6h.008v.008H7.5V6Zm2.25 0h.008v.008H9.75V6Z" />
                             </svg>
 
-                            Tests
+                            Testes
                         </h3>
-                        <p>In this final phase, I conduct tests to ensure that everything is working as effectively as
-                            possible, and ultimately, I create documentation.</p>
+                        <p>Nesta fase final, realizo testes para garantir que tudo esteja funcionando da maneira mais eficaz possível e, por fim, crio a documentação.</p>
                     </div>
                 </div>
             </div>
@@ -122,8 +117,8 @@
     </main>
     <div class="wrapper container-2xl">
         <div class="approch__header">
-            <h2>My Works</h2>
-            <span> A few of my works.</span>
+            <h2>Meus Trabalhos</h2>
+            <span>Abaixo alguns dos meus trabalhos.</span>
         </div>
         <!-- card 1 -->
         <div class="wrapper__content">
@@ -166,8 +161,8 @@
 
     <footer class="footer">
         <div class="container-2xl">
-            <p>© 2024. All rights reserved.</p>
-            <p>Created by Leonardo Geja</p>
+            <p>© 2025. Todos os direitos reservados - Leonardo Geja</p>
+            <p>Criado por Leonardo Geja</p>
         </div>
     </footer>
 @endsection
